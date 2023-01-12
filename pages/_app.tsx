e55a7e91b4a70605220c5a938ad6fdff9dee1399
engsbergp <phil61628@aim.com>
2023-01-12 18:48:12 -0500

    remove paralax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ParallaxProvider scrollAxis="vertical">
-      <Component {...pageProps} />
-    </ParallaxProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
